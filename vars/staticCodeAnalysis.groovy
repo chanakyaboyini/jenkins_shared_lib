93ab3dc6270cec3dbf57722eb0c25ac8434631ec
chanu <chanakyaboyini02@gmail.com>
2025-01-21 22:44:22 +0530

    added credentials section

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,3 +1,5 @@
-def call(){
-    sh 'mvn clean package sonar:sonar'
+def call(credentialsId){
+    withSonarQubeEnv(credentialsId: 'Jenkins_Sonar_test_practice') {
+        sh 'mvn clean package sonar:sonar'
+    }    
 }
